030ea8068d860bb7063a58f1777b15e51a4aa09d
MugenFalco <falco-ral@yandex.ru>
2026-04-05 16:00:38 +0300

    Исправление стиля

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -25,6 +25,7 @@ public class Timetable {
             daySchedule.get(time).add(trainingSession);
         }
     }
+    
     public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(dayOfWeek);
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
